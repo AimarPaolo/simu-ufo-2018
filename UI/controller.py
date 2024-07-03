a175cc39e0a4bffc48e3ec472d67d8a257eb8fed
Paolo Aimar <s297424@studenti.polito.it>
2024-07-03 10:21:50 +0200

    soluzione da verificare

--- UI/controller.py
+++ UI/controller.py
@@ -8,10 +8,55 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def handle_hello(self, e):
-        name = self._view.txt_name.value
-        if name is None or name == "":
-            self._view.create_alert("Inserire il nome")
+    def fillDD(self):
+        for anno, navv in self._model.getYear():
+            self._view.dd_anno.options.append(ft.dropdown.Option(text=f"{anno} ({navv})", key=f"{anno}"))
+
+    def fillDDState(self):
+        self._view.dd_stato.options.clear()
+        for s in self._model._grafo.nodes:
+            self._view.dd_stato.options.append(ft.dropdown.Option(text=s))
+    def hanldle_graph(self, e):
+        self._view.txt_result.controls.clear()
+        anno = self._view.dd_anno.value
+        if anno is None:
+            self._view.create_alert("Selezionare un anno!")
             return
-        self._view.txt_result.controls.append(ft.Text(f"Hello, {name}!"))
+        self._model.buildGraph(int(anno))
+        self._view.txt_result.controls.append(ft.Text(f"Grafo correttamente creato!"))
+        nNodes, nEdges = self._model.getCaratteristiche()
+        self._view.txt_result.controls.append(ft.Text(f"Il grafo creato ha {nNodes} nodi"))
+        self._view.txt_result.controls.append(ft.Text(f"Il grafo creato ha {nEdges} archi"))
+        self.fillDDState()
+        self._view.update_page()
+
+    def handle_sequenza(self, e):
+        self._view.txt_result.controls.clear()
+        stato = self._view.dd_stato.value
+        if stato is None:
+            self._view.create_alert("Selezionare uno stato!")
+            return
+        print(self._model.getConnectedComponents(stato))
+        self._view.txt_result.controls.append(ft.Text(f"I nodi adiacenti allo stato {stato} sono:"))
+        for adiacenti in self._model.getAdiacent(stato):
+            self._view.txt_result.controls.append(ft.Text(f"{adiacenti}"))
+        conness = self._model.getConnectedComponents(stato)
+        self._view.txt_result.controls.append(ft.Text(f"I nodi connessi allo stato {stato} sono {len(conness)}:"))
+        for conn in conness:
+            self._view.txt_result.controls.append(ft.Text(f"{conn}"))
+        self._view.update_page()
+
+    def handle_analizza(self, e):
+        pass
+
+    def handle_avvistamenti(self, e):
+        self._view.txt_result.controls.clear()
+        stato = self._view.dd_stato.value
+        if stato is None:
+            self._view.create_alert("Selezionare uno stato!")
+            return
+        path = self._model.getBestPath(stato)
+        self._view.txt_result.controls.append(ft.Text(f"Percorso migliore di lunghezza {len(path)}"))
+        for s in path:
+            self._view.txt_result.controls.append(ft.Text(f"{s}"))
         self._view.update_page()
--- database/DAO.py
+++ database/DAO.py
@@ -4,3 +4,71 @@ from database.DB_connect import DBConnect
 class DAO():
     def __init__(self):
         pass
+    @staticmethod
+    def nome1():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select distinct year(s.`datetime`) as data, count(s.id) as peso
+from sighting s 
+group by year(s.`datetime`)
+order by s.`datetime` 
+"""
+        cursor.execute(query)
+        for row in cursor:
+            result.append((row["data"], row["peso"]))
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def nome2(anno):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select distinct s.state 
+from sighting s 
+where year(s.`datetime`) = %s
+order by s.state 
+"""
+        cursor.execute(query, (anno, ))
+        for row in cursor:
+            result.append(row["state"])
+            #Prodotto(**row)
+        cursor.close()
+        conn.close()
+        return result
+
+    @staticmethod
+    def nome3(anno):
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """select t1.s1 as stat1, t2.s2 as stat2
+from
+(select s.state as s1, max(s.`datetime`) as m1  
+from sighting s 
+where year(s.`datetime`) = %s
+group by s.state) as t1,
+(select s.state as s2, min(s.`datetime`) as m2
+from sighting s 
+where year(s.`datetime`) = %s
+group by s.state) as t2
+where t1.s1 != t2.s2 and t1.m1 > t2.m2
+
+"""
+
+        cursor.execute(query, (anno, anno, ))
+
+        for row in cursor:
+            result.append((row["stat1"], row["stat2"]))
+
+        cursor.close()
+        conn.close()
+        return result
